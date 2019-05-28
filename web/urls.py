c41ef9b4aaa5d5f072aa4bb489d77866e7022da6
iewad <lidawei.me@gmail.com>
2019-05-29 00:40:21 +0800

    编写后台视图测试

--- web/urls.py
+++ web/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from web.views import index
 
 urlpatterns = [
     path('', index.index, name="index"),
